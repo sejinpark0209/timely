7d1cd7b558efcb47415931b62216a115e3a48f3b
Sejin Park <sejinpark0209@gmail.com>
2020-07-25 11:45:56 -0700

    work open link automatically with counter btn example

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,12 +3,26 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      counter: 0
+    }
+    this.increaseCounter = this.increaseCounter.bind(this);
+  }
+
+  increaseCounter() {
+    const count = this.state.counter += 1;
+    this.setState({ counter: count });
   }
 
   render() {
+    console.log('count:', this.state.counter);
+    if(this.state.counter === 5) {
+      window.open("https://developer.mozilla.org/en-US/");
+    }
+
     return (
       <div>
-        hihi
+        <button onClick={this.increaseCounter} >click</button>
       </div>
     );
   }
